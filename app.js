9f01443d11e3ce825fb5629c4e968b8e8a478914
rupalikgaikwad <rupalikgaikwad@gmail.com>
2018-12-16 23:11:23 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -23,24 +23,64 @@ app.get('/new', newf.new);
 
 app.get('/fetchdata', function(req, res, next) {
 	console.log("inside fetchdata");
-  //var id = req.query.id;
-  var docs = require("./public/jsoncontent.json");
+  var pDocPath = req.query.DocPath;
+  var pDocType = req.query.DocType;
+  var pUser = req.query.User;
+  var pSystem = req.query.System;
+  console.log(req.query);
+  var docs = require("./public/SourceSystem.json");
 // Get Value from JSON
-  console.log("User Name:", docs[0].DocType);
-  res.send(docs);
-  //res.render('Search',{"MKData": docs});
+  var result = docs.filter(function(v, i) {
+	  var isValid = true;
+	  if((pDocPath != null) && (pDocPath != "") && (v["DocPath"].toUpperCase() != pDocPath.toUpperCase())){
+		  isValid = false;
+	  }
+	  //console.log("pDocPath ",pDocPath+isValid);
+	  if((pDocType != null) && (pDocType != "") && (v["DocType"].toUpperCase() != pDocType.toUpperCase())){
+		  isValid = false;
+	  }
+	  //console.log("pDocType ",pDocPath+isValid);
+	  if((pUser != null) && (pUser != "") && (v["User"].toUpperCase() != pUser.toUpperCase())){
+		  isValid = false;
+	  }
+	  //console.log("pUser ",pDocPath+isValid);
+	  if((pSystem != null) && (pSystem != "") && (v["System"].toUpperCase() != pSystem.toUpperCase())){
+		  isValid = false;
+	  }
+	  /*if(v["SystemLevel"] != 0){
+		  isValid = false;
+	  }*/
+	  //console.log("pSystem ",pDocPath+isValid);
+    return isValid;
+  })
+  res.send(result);
   
 });
 
-app.get('/clicks', (req, res) => {
-  console.log("inside get Clicks");
-  //var id = req.query.id;
-  var docs = require("./public/jsoncontent.json");
+
+app.get('/getDocData', function(req, res, next) {
+  try{
+  console.log("inside getDocData");
+  var id = req.query.DocPath;
+  var docs = require("./public/SourceSystem.json");
+  console.log(docs);
+  var feedingSys = "";
+  var result = docs.filter(function(v, i) {
+	if((v["SystemLevel"] ==0)&&(v["DocPath"].toUpperCase() == id.toUpperCase())){
+		feedingSys = v["SourceSystem"];
+	}
+    return (v["DocPath"].toUpperCase() == id.toUpperCase());
+  })
+  result.push({"ParentSystem":feedingSys,"SourceSystem":"XDS"});
 // Get Value from JSON
-  console.log("User Name:", docs[0].DocType);
-  //res.render('Search',{"MKData": docs});
-  res.send(docs);
-  });
+  console.log("update Name:", id);
+  
+  res.render('update',{MKData:result});
+  }catch(ex)
+  { console.log("server error:",ex);}
+  
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
